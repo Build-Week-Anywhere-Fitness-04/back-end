177dcf94e97f15b1e7f7f860ec702123e27a9b38
fbzr <fabricio.spb@gmail.com>
2020-04-26 21:01:10 -0700

    change seed files to see if it works in production

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# back-end
+# Fitness Anywhere API
--- data/seeds/01-instructors.js
+++ data/seeds/01-instructors.js
@@ -3,7 +3,13 @@ const instructors = require('../seed-data/instructors');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('instructors').truncate()
+  return process.env.NODE_ENV 
+  ? knex.raw('TRUNCATE TABLE instructors CASCADE')
+  .then(function () {
+    // Inserts seed entries
+    return knex('instructors').insert(instructors);
+  })
+  : knex('instructors').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('instructors').insert(instructors);
